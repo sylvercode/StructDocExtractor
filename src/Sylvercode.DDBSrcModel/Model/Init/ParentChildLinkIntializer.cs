258a3837c75694850c4bed13901ccdc131e489f8
Sylvercode <sylver.dev@outlook.com>
2024-08-25 00:31:26 -0400

    Flush collection after InitializeParentChildLink and no longer usable.

--- src/Sylvercode.DDBSrcModel/Model/Init/ParentChildLinkIntializer.cs
+++ src/Sylvercode.DDBSrcModel/Model/Init/ParentChildLinkIntializer.cs
@@ -6,20 +6,27 @@ public class ParentChildLinkIntializer<P, C>(P parent) :
     where P : class, ISrcNodeHolderInitializer<C>
     where C : class, ISrcNode
 {
-    private readonly List<C> _childrens = [];
+    private List<C>? _childrens = [];
 
     public void AddChild(ISrcNodeIntializer child)
     {
+        if (_childrens is null)
+            throw new InvalidOperationException($"{nameof(InitializeParentChildLink)} Have been already called.");
+
         child.CheckParentTypeOrThrow(parent);
         _childrens.Add(parent.CastAsChildTypeOrThrow(child));
     }
 
     public IEnumerable<C> ChildrenToAdd()
-        => _childrens;
+        => _childrens ?? Enumerable.Empty<C>();
 
     public void InitializeParentChildLink()
     {
+        if (_childrens is null)
+            throw new InvalidOperationException($"{nameof(InitializeParentChildLink)} Have been already called.");
+
         parent.SetContent(_childrens);
         _childrens.ForEach(c => ((ISrcNodeIntializer)c).SetParent(parent));
+        _childrens = null;
     }
 }
