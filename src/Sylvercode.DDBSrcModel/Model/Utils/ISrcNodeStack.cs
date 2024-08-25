1f55239b52136ef12f40df42ceb1895ebc76046d
Sylvercode <sylver.dev@outlook.com>
2024-08-25 14:31:49 -0400

    Add Debugger display

--- src/Sylvercode.DDBSrcModel/Model/Utils/ISrcNodeStack.cs
+++ src/Sylvercode.DDBSrcModel/Model/Utils/ISrcNodeStack.cs
@@ -1,11 +1,16 @@
+using System.Diagnostics;
+
 namespace Sylvercode.DDBSrcModel.Model.Utils;
 
 public interface ISrcNodeStack : IEnumerable<ISrcNodeStack.Entry>
 {
+    [DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
     public readonly struct Entry(int depth, ISrcNode node)
     {
         public int Depth => depth;
         public ISrcNode Node => node;
+
+        private string GetDebuggerDisplay() => $"{Depth}: {Node.Id}";
     }
 
     public Entry? Peek();
