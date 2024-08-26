46cdb4f94ad284037db0cc94ad52073ed0b90550
Sylvercode <sylver.dev@outlook.com>
2024-08-26 17:54:34 -0400

    Add TaskIndex tests, correct bugs and TaskIndex implements IReadOnlyList

--- src/Sylvercode.DDBSrcModel/Extraction/TaskIndex.cs
+++ src/Sylvercode.DDBSrcModel/Extraction/TaskIndex.cs
@@ -3,45 +3,66 @@ using System.Collections.Immutable;
 
 namespace Sylvercode.DDBSrcModel.Extraction;
 
-public readonly struct TaskIndex : IComparable<TaskIndex>
+public readonly struct TaskIndex : IComparable<TaskIndex>, IReadOnlyList<int>, IStructuralComparable, IStructuralEquatable, IEquatable<TaskIndex>
 {
     private readonly ImmutableArray<int> _indexes;
 
     private TaskIndex(ImmutableArray<int> indexes)
         => _indexes = indexes;
 
-    public TaskIndex(int index = 0) : this([index])
+    public TaskIndex() : this([0])
     { }
 
-    public TaskIndex(TaskIndex? parent, int index) : this([.. parent?._indexes ?? [], index])
+    public TaskIndex(int index) : this([index])
+    { }
+
+    public TaskIndex(TaskIndex? parent, int index = 0) : this([.. parent?._indexes ?? [], index])
     { }
 
     public static implicit operator TaskIndex(int value) => new(value);
 
-    public TaskIndex NewSubTaskIndex(int index)
+    public TaskIndex NewSubTaskIndex(int index = 0)
         => new(this, index);
 
-    public bool HasSubTaskIndex => _indexes.Length > 1;
+    public bool IsSubTaskIndex => _indexes.Length > 1;
 
-    public TaskIndex? ParentTaskIndex()
-        => HasSubTaskIndex ? new TaskIndex(_indexes[..^2]) : null;
+    int IReadOnlyCollection<int>.Count => _indexes.Length;
+
+    public int this[int index] => _indexes[index];
+
+    public TaskIndex ParentTaskIndex()
+        => IsSubTaskIndex ? new TaskIndex(_indexes[..^1])
+                          : throw new InvalidOperationException("Index is not as sub task index.");
 
     public TaskIndex Increment()
-        => new(ParentTaskIndex(), _indexes[^1] + 1);
+        => new([.. _indexes[..^1], (_indexes[^1] + 1)]);
 
     public int CompareTo(TaskIndex other) =>
         ((IStructuralComparable)_indexes).CompareTo(other._indexes, StructuralComparisons.StructuralComparer);
 
     public override bool Equals(object? obj)
-    {
-        if (obj is not TaskIndex other)
-            return false;
-
-        return ((IStructuralEquatable)_indexes).Equals(other._indexes, StructuralComparisons.StructuralEqualityComparer);
-    }
+        => (obj is TaskIndex other) && Equals(other);
 
     public override int GetHashCode() =>
         ((IStructuralEquatable)_indexes).GetHashCode(StructuralComparisons.StructuralEqualityComparer);
+
+    public IEnumerator<int> GetEnumerator()
+        => ((IEnumerable<int>)_indexes).GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => ((IEnumerable)_indexes).GetEnumerator();
+
+    public int CompareTo(object? other, IComparer comparer)
+        => ((IStructuralComparable)_indexes).CompareTo(other, comparer);
+
+    public bool Equals(object? other, IEqualityComparer comparer)
+        => ((IStructuralEquatable)_indexes).Equals(other, comparer);
+
+    public int GetHashCode(IEqualityComparer comparer)
+        => ((IStructuralEquatable)_indexes).GetHashCode(comparer);
+
+    public bool Equals(TaskIndex other)
+        => ((IStructuralEquatable)_indexes).Equals(other._indexes, StructuralComparisons.StructuralEqualityComparer);
 
     public static bool operator ==(TaskIndex left, TaskIndex right)
         => left.Equals(right);
