3e317e704a96431bbf7ff6a72480f5f61118667a
Sylvercode <987564+sylvercode@users.noreply.github.com>
2024-08-31 15:19:09 -0400

    Correct typo

--- src/Sylvercode.DDBSrcModel/Extraction/Factory/ISrcNodeFactoryProvider.cs
+++ src/Sylvercode.DDBSrcModel/Extraction/Factory/ISrcNodeFactoryProvider.cs
@@ -4,5 +4,5 @@ namespace Sylvercode.DDBSrcModel.Extraction.Factory;
 
 public interface ISrcNodeFactoryProvider<TExtractionData, TDataSelectable>
 {
-    ISrcNodeFactory<TExtractionData, TDataSelectable>? GetFactoryForStack(IStructDataStack<TDataSelectable> staskEntries);
+    ISrcNodeFactory<TExtractionData, TDataSelectable>? GetFactoryForStack(IStructDataStack<TDataSelectable> stackEntries);
 }
