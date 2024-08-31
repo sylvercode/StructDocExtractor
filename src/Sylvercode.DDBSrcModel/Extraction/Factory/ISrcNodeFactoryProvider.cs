ca51766a81f1f1206b8cefda68d915f0e9ff3d68
Sylvercode <987564+sylvercode@users.noreply.github.com>
2024-08-31 16:05:01 -0400

    Add DebugName

--- src/Sylvercode.DDBSrcModel/Extraction/Factory/ISrcNodeFactoryProvider.cs
+++ src/Sylvercode.DDBSrcModel/Extraction/Factory/ISrcNodeFactoryProvider.cs
@@ -5,4 +5,5 @@ namespace Sylvercode.DDBSrcModel.Extraction.Factory;
 public interface ISrcNodeFactoryProvider<TExtractionData, TDataSelectable>
 {
     ISrcNodeFactory<TExtractionData, TDataSelectable>? GetFactoryForStack(IStructDataStack<TDataSelectable> stackEntries);
+    string DebugName => GetType().Name;
 }
