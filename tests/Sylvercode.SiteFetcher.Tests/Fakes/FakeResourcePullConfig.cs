b228c3e3bcb6de846fd949b73e0dbbe0781d8897
Sylvercode <sylver.dev@outlook.com>
2024-09-08 16:51:32 -0400

    Add BaseResourcesTracker tests

---
+++ tests/Sylvercode.SiteFetcher.Tests/Fakes/FakeResourcePullConfig.cs
@@ -0,0 +1,9 @@
+﻿using Sylvercode.SiteFetcher.Resources;
+
+namespace Sylvercode.SiteFetcher.Tests.Fakes;
+
+public class FakeResourcePullConfig : IResourcePullConfig
+{
+    public ResourcePullType NestResult { get; set; } = ResourcePullType.NoPull;
+    public ResourcePullType GetPullType(Uri uri) => NestResult;
+}
---
+++ tests/Sylvercode.SiteFetcher.Tests/Stubs/BasicResourcesTracker.cs
@@ -0,0 +1,14 @@
+﻿using Sylvercode.SiteFetcher.Resources;
+
+namespace Sylvercode.SiteFetcher.Tests.Stubs;
+
+public class BasicResourcesTracker(ResourceDictionary resourceDictionary,
+                                   IResourcePullConfig config) : BaseResourcesTracker(resourceDictionary, config)
+{
+    protected override Uri? GetUri(object sender)
+    {
+        if (sender is Uri uri)
+            return uri;
+        return null;
+    }
+}
