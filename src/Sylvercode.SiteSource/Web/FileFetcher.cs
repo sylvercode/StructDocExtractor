685d0205d502fbccc0ded8c9a1704c1e5510fe80
Sylvercode <sylver.dev@outlook.com>
2024-09-08 21:40:46 -0400

    Add FileFetcher and ImageSelector

---
+++ src/Sylvercode.SiteSource/Web/FileFetcher.cs
@@ -0,0 +1,18 @@
+﻿namespace Sylvercode.SiteSource.Web;
+
+public class FileFetcher(HttpClient httpClient)
+{
+    public bool FetchFile(Uri uri, out byte[] imageBytes)
+    {
+        try
+        {
+            imageBytes = httpClient.GetByteArrayAsync(uri).Result;
+            return true;
+        }
+        catch (Exception)
+        {
+            imageBytes = [];
+            return false;
+        }
+    }
+}
