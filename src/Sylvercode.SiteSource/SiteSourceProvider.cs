e358b4adc3d07c63218610e844859539613a0ac8
Sylvercode <sylver.dev@outlook.com>
2024-09-08 21:19:58 -0400

    Add SiteSourceProvider

---
+++ src/Sylvercode.SiteSource/SiteSourceProvider.cs
@@ -0,0 +1,20 @@
+﻿namespace Sylvercode.SiteSource;
+
+public class SiteSourceProvider
+{
+    private class Entry(ISiteSourceProviderSelector selector, ISiteSource source)
+    {
+        public ISiteSourceProviderSelector Selector { get; } = selector;
+        public ISiteSource Source { get; } = source;
+    }
+
+    private readonly List<Entry> _sourceProviders = [];
+
+    public void Add(ISiteSourceProviderSelector selector, ISiteSource source)
+        => _sourceProviders.Add(new Entry(selector, source));
+
+    public ISiteSource GetSource(Uri uri, ISiteSource? defaultSource)
+        => _sourceProviders.FirstOrDefault(entry => entry.Selector.IsValid(uri))?.Source
+           ?? defaultSource
+           ?? throw new InvalidOperationException("No source found");
+}
