34b858b4aa3fecab4ddb852a1db5518836d0784b
Sylvercode <sylver.dev@outlook.com>
2024-09-08 21:40:22 -0400

    Remove use of HttpClient

--- src/Sylvercode.SiteSource.AngleSharp/AngleSharpWebSource.cs
+++ src/Sylvercode.SiteSource.AngleSharp/AngleSharpWebSource.cs
@@ -1,4 +1,3 @@
-
 using AngleSharp;
 using AngleSharp.Dom;
 
@@ -7,10 +6,7 @@ namespace Sylvercode.SiteSource.AngleSharp;
 public class AngleSharpWebSource(IConfiguration? config = null, IBrowsingContext? browsingContext = null)
     : BaseAngleSharpSiteSource(config, browsingContext)
 {
-    private readonly HttpClient _httpClient = new();
-
-    public override bool DataExists(Uri uri) =>
-        _httpClient.GetAsync(uri).Result.EnsureSuccessStatusCode().IsSuccessStatusCode;
+    public override bool DataExists(Uri uri) => true;
 
     protected override IDocument GetDocument(Uri uri)
         => BrowsingContext.OpenAsync(AsAngleSharpUrl(uri)).Result;
