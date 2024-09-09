3e8975f08b72fb18dd2bbf7cf8f8c489e411310d
Sylvercode <sylver.dev@outlook.com>
2024-09-08 20:48:52 -0400

    Add StructDocNodeFactoryProviderSelector

---
+++ src/Sylvercode.StructDocExtractor.AngleSharp/StructDocNodeFactoryProviderSelector.cs
@@ -0,0 +1,31 @@
+﻿using AngleSharp.Dom;
+using Sylvercode.StructDocExtractor.Extraction.Factory;
+using Sylvercode.StructDocExtractor.StdHtml.Model;
+
+namespace Sylvercode.StructDocExtractor.AngleSharp;
+
+public class StructDocNodeFactoryProviderSelector
+{
+    public interface ISelector
+    {
+        bool IsValid(IElement element);
+    }
+
+    private class Entry(ISelector selector, IStructDocNodeFactoryProvider<IElement, HtmlNodeDiscriminator> factoryProvider)
+    {
+        public ISelector Selector { get; } = selector;
+        public IStructDocNodeFactoryProvider<IElement, HtmlNodeDiscriminator> FactoryProvider { get; } = factoryProvider;
+    }
+
+    private readonly List<Entry> m_FactoryProviders = [];
+
+    public void Add(ISelector selector, IStructDocNodeFactoryProvider<IElement, HtmlNodeDiscriminator> factoryProvider)
+        => m_FactoryProviders.Add(new Entry(selector, factoryProvider));
+
+    public IStructDocNodeFactoryProvider<IElement, HtmlNodeDiscriminator> GetFactoryProvider(
+        IElement element,
+        IStructDocNodeFactoryProvider<IElement, HtmlNodeDiscriminator>? defaultFactoryProvider)
+        => m_FactoryProviders.FirstOrDefault(entry => entry.Selector.IsValid(element))?.FactoryProvider
+           ?? defaultFactoryProvider
+           ?? throw new InvalidOperationException("No factory provider found");
+}
