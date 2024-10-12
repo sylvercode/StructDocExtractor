7352e1b1d46f2a00866faa746ac5e86559a451a3
Sylvercode <sylver.dev@outlook.com>
2024-10-12 12:01:28 -0400

    Add ResourceDataExtractor logger

--- src/Sylvercode.SiteExtractor/Resources/Processors/ResourceDataExtractor.cs
+++ src/Sylvercode.SiteExtractor/Resources/Processors/ResourceDataExtractor.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Sylvercode.SiteExtractor.Sources;
 using Sylvercode.SiteExtractor.Store;
 using Sylvercode.SiteExtractor.UriUtils;
@@ -7,13 +9,14 @@ using Sylvercode.StructDocExtractor.Serialization;
 
 namespace Sylvercode.SiteExtractor.Resources.Processors;
 
-public class ResourceDataExtractor<TExtractionData>(
+public partial class ResourceDataExtractor<TExtractionData>(
     ISiteSource<TExtractionData> siteSource,
     IExtractor<TExtractionData> extractor,
     IDataStore dataStore,
     IStructDocSerializer serisalizer,
     IUriTranslater? uriTranslater = null,
-    IReferencerUpdater? referencerUpdater = null) : IResourceDataExtractor<TExtractionData>
+    IReferencerUpdater? referencerUpdater = null,
+    ILogger<ResourceDataExtractor<TExtractionData>>? logger = null) : IResourceDataExtractor<TExtractionData>
 {
     private class ExtractionTaskObserver : IObserver<ExtractionTask>
     {
@@ -28,19 +31,34 @@ public class ResourceDataExtractor<TExtractionData>(
         }
     }
 
+    private readonly ILogger<ResourceDataExtractor<TExtractionData>> _logger = logger ?? new NullLogger<ResourceDataExtractor<TExtractionData>>();
+
     private IUriTranslater UriTransler { get; } = uriTranslater ?? new UriBaseTranslater(siteSource.BaseUri, dataStore.BaseUri);
 
     public IResourceProcessorResult Extract(Resource resource, IReadOnlyDictionary<Uri, Resource> trackedResources)
     {
+        using var scope = _logger.BeginScope((ResourceUri: resource.Uri, SiteUri: siteSource.BaseUri));
         TExtractionData extractionData = siteSource.GetData(resource.Uri);
         if (extractionData is null)
+        {
+            LogNoDataFromSource();
             return new FinishedProcessResult(this, resource);
+        }
 
         ExtractionTaskObserver observer = new();
         ExtractionResult result = extractor.Extract(extractionData, observer);
 
         if (result.StructDocNodes.Count == 0)
+        {
+            LogNoNodeFromSource();
             return new FinishedProcessResult(this, resource);
+        }
+
+        if (_logger.IsEnabled(LogLevel.Debug))
+        {
+            foreach (IStructDocReferencer referencer in observer.Referencers)
+                LogReferencer(referencer.GetReference());
+        }
 
         return new DataExtractedProcessorResult<TExtractionData>(this, resource, trackedResources, result, UriTransler, observer.Referencers);
     }
@@ -55,6 +73,22 @@ public class ResourceDataExtractor<TExtractionData>(
 
         return new FinishedProcessResult(this, lastResult.Resource);
     }
+
+    [LoggerMessage(
+        Level = LogLevel.Information,
+        Message = "No data found.")]
+    private partial void LogNoDataFromSource();
+
+    [LoggerMessage(
+        Level = LogLevel.Information,
+        Message = "No node found.")]
+    private partial void LogNoNodeFromSource();
+
+    [LoggerMessage(
+        SkipEnabledCheck = true,
+        Level = LogLevel.Debug,
+        Message = "Referencer: {Referencer}.")]
+    private partial void LogReferencer(string referencer);
 
     #region IResourceProcessor
     public IResourceProcessorResult Process(Resource resource, IReadOnlyDictionary<Uri, Resource> trackedResources) => Extract(resource, trackedResources);
