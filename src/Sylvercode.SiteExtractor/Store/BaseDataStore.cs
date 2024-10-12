4711a9c220b84546fbdad91e75184cec1ed13eca
Sylvercode <sylver.dev@outlook.com>
2024-10-12 13:31:27 -0400

    Add DirectoryDataStore logger

--- src/Sylvercode.SiteExtractor/Store/BaseDataStore.cs
+++ src/Sylvercode.SiteExtractor/Store/BaseDataStore.cs
@@ -1,9 +1,14 @@
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
 
 namespace Sylvercode.SiteExtractor.Store;
 
-public abstract class BaseDataStore(IOptions<SiteExtractorOptions> options) : IDataStore
+public abstract partial class BaseDataStore(
+    IOptions<SiteExtractorOptions> options,
+    ILogger? logger = null) : IDataStore
 {
+    private readonly ILogger _logger = logger ?? NullLogger.Instance;
     public Uri BaseUri => new(options.Value.OutputDirectory);
     public abstract Stream GetStream(Uri uri);
     public virtual StreamWriter GetStreamWriter(Uri uri) => new(GetStream(uri));
@@ -11,12 +16,26 @@ public abstract class BaseDataStore(IOptions<SiteExtractorOptions> options) : ID
     protected Uri GetCompleteUri(Uri uri)
     {
         if (BaseUri.IsBaseOf(uri))
+        {
+            LogUriAlreadyComplete(uri);
             return uri;
+        }
 
         Uri completeUri = new(BaseUri, uri);
         if (!BaseUri.IsBaseOf(completeUri))
             throw new ArgumentException("Uri is not a child of the base uri", nameof(uri));
 
+        LogUriCompleted(uri, completeUri);
         return completeUri;
     }
+
+    [LoggerMessage(
+        Level = LogLevel.Debug,
+        Message = "Uri {Uri} is already complete")]
+    private partial void LogUriAlreadyComplete(Uri uri);
+
+    [LoggerMessage(
+        Level = LogLevel.Debug,
+        Message = "Uri {OriginalUri} completd to {Uri}")]
+    private partial void LogUriCompleted(Uri originalUri, Uri uri);
 }
