8c1500e82545f6b8a267e96ef08e3961f586122f
Sylvercode <sylver.dev@outlook.com>
2024-10-14 18:46:47 -0400

    MarkdownReferencerUpdaterTests stub

---
+++ tests/Sylvercode.SiteExtractor.Markdown.Tests/MarkdownReferencerUpdaterTests.cs
@@ -0,0 +1,17 @@
+using Sylvercode.SiteExtractor.Resources;
+
+namespace Sylvercode.SiteExtractor.Markdown.Tests;
+
+public class MarkdownReferencerUpdaterTests_UpdateReferencers
+{
+    [Fact]
+    public void FragmentOnly_Untouched()
+    {
+        // Given
+        ResourceDictionary resources = [];
+    
+        // When
+    
+        // Then
+    }
+}
