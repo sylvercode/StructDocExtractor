cc8f9bf539ca6c166a5f1025c60020c89a090208
Sylvercode <sylver.dev@outlook.com>
2024-10-15 16:06:39 -0400

    Add AngleProcessTaskResultBuilder

--- src/Sylvercode.StructDocExtractor.AngleSharp/Extraction/AngleProcessTaskResultBuilder.cs
+++ src/Sylvercode.StructDocExtractor.AngleSharp/Extraction/AngleProcessTaskResultBuilder.cs
@@ -7,27 +7,40 @@ namespace Sylvercode.StructDocExtractor.AngleSharp.Extraction;
 public class AngleProcessTaskResultBuilder(IElement sourceNode) : ProcessTaskResultBuilder<IElement, HtmlNodeDiscriminator>
 {
     public AngleProcessTaskResultBuilder WithSubTaskByAll(string selector)
+        => WithSubTaskByAll(sourceNode, selector);
+
+    public AngleProcessTaskResultBuilder WithSubTaskByAll(IElement element, string selector)
     {
-        WithSubTasks(sourceNode.QuerySelectorAll(selector));
+        WithSubTasks(element.QuerySelectorAll(selector));
         return this;
     }
+
     public AngleProcessTaskResultBuilder WithSubTaskBySingle(string selector)
+        => WithSubTaskBySingle(sourceNode, selector);
+
+    public AngleProcessTaskResultBuilder WithSubTaskBySingle(IElement element, string selector)
     {
-        IElement? subElement = sourceNode.QuerySelector(selector);
+        IElement? subElement = element.QuerySelector(selector);
         if (subElement is not null)
             WithSubTask(subElement);
         return this;
     }
 
     public AngleProcessTaskResultBuilder WithExtraTaskByAll(string selector)
+        => WithExtraTaskByAll(sourceNode, selector);
+
+    public AngleProcessTaskResultBuilder WithExtraTaskByAll(IElement element, string selector)
     {
-        WithExtraTasks(sourceNode.QuerySelectorAll(selector));
+        WithExtraTasks(element.QuerySelectorAll(selector));
         return this;
     }
-    
+
     public AngleProcessTaskResultBuilder WithExtraTaskBySingle(string selector)
+        => WithExtraTaskBySingle(sourceNode, selector);
+
+    public AngleProcessTaskResultBuilder WithExtraTaskBySingle(IElement element, string selector)
     {
-        IElement? extraElement = sourceNode.QuerySelector(selector);
+        IElement? extraElement = element.QuerySelector(selector);
         if (extraElement is not null)
             WithExtraTask(extraElement);
         return this;
