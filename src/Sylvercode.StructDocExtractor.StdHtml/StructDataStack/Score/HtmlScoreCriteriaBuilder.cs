30d420bd44955d319b4af04a48b144b356e90bcc
Sylvercode <sylver.dev@outlook.com>
2024-10-17 19:31:09 -0400

    Extract BaseScoreCriteriaSetsBuilder

--- src/Sylvercode.StructDocExtractor.StdHtml/StructDataStack/Score/HtmlScoreCriteriaBuilder.cs
+++ src/Sylvercode.StructDocExtractor.StdHtml/StructDataStack/Score/HtmlScoreCriteriaBuilder.cs
@@ -1,24 +1,17 @@
-using System.Reflection;
 using Sylvercode.StructDocExtractor.StdHtml.Model;
 using Sylvercode.StructDocExtractor.StructDataStack.Score;
 
 namespace Sylvercode.StructDocExtractor.StdHtml.StructDataStack.Score;
 
-public class HtmlScoreCriteriaSetsBuilder
+public class HtmlScoreCriteriaSetsBuilder : BaseScoreCriteriaSetsBuilder<HtmlNodeDiscriminator, HtmlScoreCriteriaSetsBuilder>
 {
-    public const int IdScorePriority = 1;
-    public const int StyleClassScorePriority = 2;
-    public const int TagNameScorePriority = 3;
+    public const int IdScorePriority = 2;
+    public const int StyleClassScorePriority = 1;
+    public const int TagNameScorePriority = 0;
 
     public static Func<HtmlNodeDiscriminator, string> IdGetter { get; } = d => d.Id;
     public static Func<HtmlNodeDiscriminator, string[]> StyleClassGetter { get; } = d => d.StyleClass;
     public static Func<HtmlNodeDiscriminator, string> TagNameGetter { get; } = d => d.TagName;
-
-    private readonly List<NodeScoreCriteriaSet<HtmlNodeDiscriminator>.NodeScoreCriterion> _criterionList = [];
-
-    private readonly List<NodeScoreCriteriaSet<HtmlNodeDiscriminator>.NodeScoreCriteria> _criteriaList = [];
-
-    private readonly List<NodeScoreCriteriaSet<HtmlNodeDiscriminator>> _criteriaSetList = [];
 
 
     public HtmlScoreCriteriaSetsBuilder WithId(string id)
@@ -74,80 +67,13 @@ public class HtmlScoreCriteriaSetsBuilder
         AddTagCriterion(NewAnyCriterion<StaticValueMatcher>(tagName));
         return this;
     }
-    public HtmlScoreCriteriaSetsBuilder NextCriteria()
-    {
-        if (_criterionList.Count == 0)
-            return this;
-        _criteriaList.Add(new(_criterionList));
-        _criterionList.Clear();
-        return this;
-    }
-
-    public HtmlScoreCriteriaSetsBuilder NextCriteriaSet()
-    {
-        NextCriteria();
-        if (_criteriaList.Count == 0)
-            return this;
-        _criteriaSetList.Add(new(_criteriaList.ToArray()));
-        _criteriaList.Clear();
-        return this;
-    }
-
-    public HtmlScoreCriteriaSetsBuilder WithNextCriteriaSets(
-        IEnumerable<NodeScoreCriteriaSet<HtmlNodeDiscriminator>> criteriaSets)
-    {
-        NextCriteriaSet();
-        _criteriaSetList.AddRange(criteriaSets);
-        return this;
-    }
-
-    public List<NodeScoreCriteriaSet<HtmlNodeDiscriminator>> Build()
-    {
-        NextCriteriaSet();
-        return _criteriaSetList;
-    }
-
-    private static ValuesMatcherAll NewAllCriterion<TEquatable>(string[] valueMatchers)
-        where TEquatable : IEquatable<string>
-    {
-        return new ValuesMatcherAll(valueMatchers.Select(NewEquatable<TEquatable>).ToArray());
-    }
-
-    private static ValuesMatcherAny NewAnyCriterion<TEquatable>(string[] valueMatchers)
-        where TEquatable : IEquatable<string>
-    {
-        return new ValuesMatcherAny(valueMatchers.Select(NewEquatable<TEquatable>).ToArray());
-    }
-
-    private static IEquatable<string> NewEquatable<TEquatable>(string value)
-        where TEquatable : IEquatable<string>
-    {
-        ConstructorInfo ctor = typeof(TEquatable).GetConstructor([typeof(string)])
-            ?? throw new InvalidOperationException();
-        return (IEquatable<string>)ctor.Invoke([value]);
-    }
 
     private void AddIdCriterion(IValueMatcher valueMatcher)
-    {
-        CheckRepeatedCriterion(IdScorePriority);
-        _criterionList.Add(new(IdScorePriority, valueMatcher, IdGetter));
-    }
+        => AddCriterion(IdScorePriority, valueMatcher, IdGetter);
 
     private void AddStyleClassCriterion(IValueMatcher valueMatcher)
-    {
-        CheckRepeatedCriterion(StyleClassScorePriority);
-        _criterionList.Add(new(StyleClassScorePriority, valueMatcher, StyleClassGetter));
-    }
+        => AddCriterion(StyleClassScorePriority, valueMatcher, StyleClassGetter);
 
     private void AddTagCriterion(IValueMatcher valueMatcher)
-    {
-        CheckRepeatedCriterion(TagNameScorePriority);
-        _criterionList.Add(new(TagNameScorePriority, valueMatcher, TagNameGetter));
-    }
-
-    private void CheckRepeatedCriterion(int priority)
-    {
-        if (_criterionList.Any(c => c.Priority == priority))
-            throw new InvalidOperationException("Criterion of equal priority already added");
-    }
+        => AddCriterion(TagNameScorePriority, valueMatcher, TagNameGetter);
 }
---
+++ src/Sylvercode.StructDocExtractor/StructDataStack/Score/BaseScoreCriteriaSetsBuilder.cs
@@ -0,0 +1,73 @@
+using System.Reflection;
+
+namespace Sylvercode.StructDocExtractor.StructDataStack.Score;
+
+public class BaseScoreCriteriaSetsBuilder<TDiscriminator, TBuilder>
+    where TBuilder : BaseScoreCriteriaSetsBuilder<TDiscriminator, TBuilder>
+{
+    private readonly List<NodeScoreCriteriaSet<TDiscriminator>.NodeScoreCriterion> _criterionList = [];
+
+    private readonly List<NodeScoreCriteriaSet<TDiscriminator>.NodeScoreCriteria> _criteriaList = [];
+
+    private readonly List<NodeScoreCriteriaSet<TDiscriminator>> _criteriaSetList = [];
+
+    public TBuilder NextCriteria()
+    {
+        if (_criterionList.Count == 0)
+            return (TBuilder)this;
+        _criteriaList.Add(new(_criterionList));
+        _criterionList.Clear();
+        return (TBuilder)this;
+    }
+
+    public TBuilder NextCriteriaSet()
+    {
+        NextCriteria();
+        if (_criteriaList.Count == 0)
+            return (TBuilder)this;
+        _criteriaSetList.Add(new(_criteriaList.ToArray()));
+        _criteriaList.Clear();
+        return (TBuilder)this;
+    }
+
+    public TBuilder WithNextCriteriaSets(
+        IEnumerable<NodeScoreCriteriaSet<TDiscriminator>> criteriaSets)
+    {
+        NextCriteriaSet();
+        _criteriaSetList.AddRange(criteriaSets);
+        return (TBuilder)this;
+    }
+
+    public List<NodeScoreCriteriaSet<TDiscriminator>> Build()
+    {
+        NextCriteriaSet();
+        return _criteriaSetList;
+    }
+
+    protected static ValuesMatcherAll NewAllCriterion<TEquatable>(string[] valueMatchers)
+        where TEquatable : IEquatable<string>
+        => new(valueMatchers.Select(NewEquatable<TEquatable>).ToArray());
+
+    protected static ValuesMatcherAny NewAnyCriterion<TEquatable>(string[] valueMatchers)
+        where TEquatable : IEquatable<string>
+        => new(valueMatchers.Select(NewEquatable<TEquatable>).ToArray());
+
+    protected static IEquatable<string> NewEquatable<TEquatable>(string value)
+        where TEquatable : IEquatable<string>
+    {
+        ConstructorInfo ctor = typeof(TEquatable).GetConstructor([typeof(string)])
+            ?? throw new InvalidOperationException();
+        return (IEquatable<string>)ctor.Invoke([value]);
+    }
+
+    protected void AddCriterion(int priority, IValueMatcher matcher, Func<TDiscriminator, string> evaluator)
+        => AddCriterion(priority, matcher, e => [evaluator.Invoke(e)]);
+
+    protected void AddCriterion(int priority, IValueMatcher matcher, Func<TDiscriminator, IEnumerable<string>> evaluator)
+    {
+        if (_criterionList.Any(c => c.Priority == priority))
+            throw new InvalidOperationException("Criterion of equal priority already added");
+
+        _criterionList.Add(new NodeScoreCriteriaSet<TDiscriminator>.NodeScoreCriterion(priority, matcher, evaluator));
+    }
+}
