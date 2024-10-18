24fd2c75a1becca7adfa5b5190c1d9d89aeb03da
Sylvercode <sylver.dev@outlook.com>
2024-10-17 20:12:39 -0400

    Add HtmlParagraphFactory

---
+++ src/Sylvercode.StructDocExtractor.AngleSharp/Extraction/Factory/HtmlParagraphFactory.cs
@@ -0,0 +1,22 @@
+using AngleSharp.Dom;
+using Sylvercode.StructDocExtractor.StdHtml.Model;
+using Sylvercode.StructDocExtractor.StdHtml.StructDataStack.Score;
+using Sylvercode.StructDocExtractor.StructDataStack.Score;
+
+
+namespace Sylvercode.StructDocExtractor.AngleSharp.Extraction.Factory;
+
+public class HtmlParagraphFactory : BaseAngleNodeFactory
+{
+    public override List<NodeScoreCriteriaSet<HtmlNodeDiscriminator>>? DefaultSelector { get; } =
+        new HtmlScoreCriteriaSetsBuilder()
+            .WithTagName(TagNames.P)
+            .Build();
+
+    protected override bool BuildFromElement(AngleProcessTaskResultBuilder resultBuilder, IElement node)
+    {
+        resultBuilder.WithNode<HtmlParagraph>();
+        resultBuilder.WithChildNodesAsSubTasks();
+        return true;
+    }
+}
--- src/Sylvercode.StructDocExtractor.StdHtml/Model/HtmlParagraph.cs
+++ src/Sylvercode.StructDocExtractor.StdHtml/Model/HtmlParagraph.cs
@@ -1,9 +1,10 @@
 using Sylvercode.StructDocExtractor.Model.Base;
 
-namespace Sylvercode.StructDocExtractor.StdHtml.Model
-{
-    public class HtmlParagraph(string id = "") : BaseStructDocBlockWithAnyParentAndContent(id)
-    {
+namespace Sylvercode.StructDocExtractor.StdHtml.Model;
 
+public class HtmlParagraph(string id) : BaseStructDocBlockWithAnyParentAndContent(id)
+{
+    public HtmlParagraph() : this(string.Empty)
+    {
     }
 }
