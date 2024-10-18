543b1399a7515623293a840901a76bea36bb6264
Sylvercode <sylver.dev@outlook.com>
2024-10-17 20:09:43 -0400

    Add HtmlListItemFactory

--- src/Sylvercode.StructDocExtractor.StdHtml/Model/HtmlListItem.cs
+++ src/Sylvercode.StructDocExtractor.StdHtml/Model/HtmlListItem.cs
@@ -2,7 +2,9 @@ using Sylvercode.StructDocExtractor.Model.Base;
 
 namespace Sylvercode.StructDocExtractor.StdHtml.Model;
 
-public class HtmlListItem(string id = "") : BaseStructDocBlockWithAnyContent<HtmlList>(id)
+public class HtmlListItem(string id) : BaseStructDocBlockWithAnyContent<HtmlList>(id)
 {
-
+    public HtmlListItem() : this(string.Empty)
+    {
+    }
 }
