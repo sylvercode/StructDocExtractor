15a768f30ec738d213c5a167a9b17027dd81e7cd
Sylvercode <sylver.dev@outlook.com>
2024-10-17 21:40:37 -0400

    Add HtmlTableBodyFactory

---
+++ src/Sylvercode.StructDocExtractor.AngleSharp/Extraction/Factory/HtmlTableBodyFactory.cs
@@ -0,0 +1,24 @@
+using AngleSharp.Dom;
+using Sylvercode.StructDocExtractor.StdHtml.Model;
+using Sylvercode.StructDocExtractor.StdHtml.StructDataStack.Score;
+using Sylvercode.StructDocExtractor.StructDataStack.Score;
+
+namespace Sylvercode.StructDocExtractor.AngleSharp.Extraction.Factory;
+
+public class HtmlTableBodyFactory : BaseAngleNodeFactory
+{
+    public static List<NodeScoreCriteriaSet<HtmlNodeDiscriminator>> Selector { get; } =
+        new HtmlScoreCriteriaSetsBuilder()
+            .WithTagName(TagNames.Tbody)
+            .AndParentCriteriaSets(HtmlTableFactory.Selector)
+            .BuildSets();
+
+    public override List<NodeScoreCriteriaSet<HtmlNodeDiscriminator>>? DefaultSelector { get; } = Selector;
+
+    protected override bool BuildFromElement(AngleProcessTaskResultBuilder resultBuilder, IElement node)
+    {
+        resultBuilder.WithNode<HtmlTableBody>();
+        resultBuilder.WithSubTaskByAll(TagNames.Tr);
+        return true;
+    }
+}
