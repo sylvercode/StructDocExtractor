caa427a0d692b20f9b8c73b5a8b25a931dd618ba
Sylvercode <sylver.dev@outlook.com>
2024-10-18 21:07:59 -0400

    Coorect compile error for missing TextWriterProvider

--- tests/Sylvercode.SiteExtractor.Tests/Mocks/StructDocSerializerMock.cs
+++ tests/Sylvercode.SiteExtractor.Tests/Mocks/StructDocSerializerMock.cs
@@ -5,6 +5,8 @@ namespace Sylvercode.SiteExtractor.Tests.Mocks;
 
 public class StructDocSerializerMock(MockCallTracker tracker) : IStructDocSerializer
 {
+    public ITextWriterProvider? TextWriterProvider => null;
+
     public void Serialize(TextWriter stream, IStructDocNode rootData)
     {
         tracker.TrackCall(this, nameof(Serialize), [stream, rootData]);
