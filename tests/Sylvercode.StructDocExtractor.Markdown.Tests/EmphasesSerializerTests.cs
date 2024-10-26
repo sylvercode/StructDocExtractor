b46239bf0691ccb52605e388315ad1d19a3b7b4f
Sylvercode <sylver.dev@outlook.com>
2024-10-25 21:21:15 -0400

    Add EmphasesSerializer tests

---
+++ tests/Sylvercode.StructDocExtractor.Markdown.Tests/EmphasesSerializerTests.cs
@@ -0,0 +1,94 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Sylvercode.StructDocExtractor.Markdown.Serialization;
+using Sylvercode.StructDocExtractor.Markdown.Serialization.Writer;
+using Sylvercode.StructDocExtractor.Model.Init;
+using Sylvercode.StructDocExtractor.Serialization;
+using Sylvercode.StructDocExtractor.StdHtml.Model;
+
+namespace Sylvercode.StructDocExtractor.Markdown.Tests;
+
+public class EmphasesSerializerTests
+{
+    private static IHost GetHost()
+    {
+        return Host.CreateDefaultBuilder()
+            .ConfigureServices((_, services) =>
+            {
+                services.AddMarkdownStreamWriterProvider();
+                services.AddStructDocSerializer();
+                services.AddSerializer<HtmlEmphases, EmphasesSerializer>();
+                services.AddSerializer<HtmlStrong, StrongSerializer>();
+                services.AddSerializer<HtmlDiv, DivSerializer>();
+                services.AddSerializer<PlainTextNode, PlainTextSerializer>();
+                services.AddSingleton<StringTextWriter<MarkdownStreamWriter>>();
+            }).Build();
+    }
+
+    [Theory]
+    [InlineData("Hello,")]
+    [InlineData("Hello, ")]
+    public void StandaloneStyle(string source)
+    {
+        // Given
+        IHost host = GetHost();
+        IStructDocSerializer serializer = host.Services.GetRequiredService<IStructDocSerializer>();
+        StringTextWriter<MarkdownStreamWriter> stringTextWriter = host.Services.GetRequiredService<StringTextWriter<MarkdownStreamWriter>>();
+        stringTextWriter.Writer.Write(source);
+
+        HtmlDiv div = new();
+        div.MakeARoot();
+        div.InitWithContent(c =>
+        {
+            c.Add<HtmlEmphases>()
+                .InitWithContent(c =>
+                {
+                    c.Add(new PlainTextNode("World"));
+                });
+        });
+
+
+        // When
+        serializer.Serialize(stringTextWriter.Writer, div);
+
+        // Then
+        Assert.Equal($"Hello, *World*", stringTextWriter.GetResult());
+        Assert.Equal(IndentedStreamWriter.PedingOperationType.Word, stringTextWriter.Writer.PendingOperation);
+    }
+
+    [Theory]
+    [InlineData("Hello,")]
+    [InlineData("Hello, ")]
+    public void CompositStyle(string source)
+    {
+        // Given
+        IHost host = GetHost();
+        IStructDocSerializer serializer = host.Services.GetRequiredService<IStructDocSerializer>();
+        StringTextWriter<MarkdownStreamWriter> stringTextWriter = host.Services.GetRequiredService<StringTextWriter<MarkdownStreamWriter>>();
+        stringTextWriter.Writer.Write(source);
+
+        HtmlDiv div = new();
+        div.MakeARoot();
+        div.InitWithContent(c =>
+        {
+            c.Add<HtmlStrong>()
+                .InitWithContent(c =>
+                {
+                    c.Add<HtmlEmphases>()
+                        .InitWithContent(c =>
+                        {
+                            c.Add(new PlainTextNode("World"));
+                        });
+                });
+        });
+
+
+        // When
+        serializer.Serialize(stringTextWriter.Writer, div);
+
+        // Then
+        Assert.Equal($"Hello, ***World***", stringTextWriter.GetResult());
+        Assert.Equal(IndentedStreamWriter.PedingOperationType.Word, stringTextWriter.Writer.PendingOperation);
+    }
+
+}
