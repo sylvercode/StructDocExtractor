99edc8171b067e30e13721cfeab374fccf4df191
Sylvercode <sylver.dev@outlook.com>
2026-09-08 22:31:54 -0400

    Correct branch fusion error

--- tests/Sylvercode.SiteExtractor.Tests/ImageUriMatcherTests.cs
+++ tests/Sylvercode.SiteExtractor.Tests/ImageUriMatcherTests.cs
@@ -42,36 +42,3 @@ public class ImageUriMatcherTests_IsMatching
         Assert.False(result);
     }
 }
-    [InlineData("https://example.com/image.jpeg")]
-    [InlineData("https://example.com/image.png")]
-    [InlineData("https://example.com/image.gif")]
-    [InlineData("https://example.com/image.bmp")]
-    [InlineData("https://example.com/image.webp")]
-    [InlineData("https://example.com/image.svg")]
-    public void ValidExtention_ReturnTrue(string uri)
-    {
-        // Given
-        ImageUriMatcher matcher = new();
-
-        // When
-        bool result = matcher.IsMatching(new Uri(uri));
-
-        // Then
-        Assert.True(result);
-    }
-
-    [Theory]
-    [InlineData("https://example.com/image.txt")]
-    [InlineData("https://example.com/image")]
-    public void InvalidExtention_ReturnFalse(string uri)
-    {
-        // Given
-        ImageUriMatcher matcher = new();
-
-        // When
-        bool result = matcher.IsMatching(new Uri(uri));
-
-        // Then
-        Assert.False(result);
-    }
-}
